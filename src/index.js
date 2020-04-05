5f017bc0b100f99f4edf679f74d4857e4a646fbb
Tim Li <urumican@163.com>
2020-04-05 09:22:51 +0800

    expose config files

--- src/index.js
+++ src/index.js
@@ -23,6 +23,7 @@ class App extends React.Component {
   }
 
   getMenu = () => {
+    console.log('getMenu')
     return (
       <Menu theme='dark'>
         <Menu.Item key="setting:1">Option 1</Menu.Item>
@@ -33,6 +34,7 @@ class App extends React.Component {
 
   getContent = () => {
     let menu = this.getMenu()
+    console.log('getContent')
     return (
       <Dropdown overlay={menu} trigger={['click']}>
         <Button style={{ marginLeft: 8 }}>
@@ -42,21 +44,45 @@ class App extends React.Component {
     )
   }
 
+  componentDidMount = () => {
+    console.log('component did mount')
+  }
+
   render() {
+    let sel = window.getSelection()
+    document.getElementById('my-text-element').addEventListener('click', function(e) {
+      let myElement = document.getElementById('my-text-element');
+      console.log(sel)
+      console.log(myElement.selectionStart)
+      console.log(document.activeElement.selectionStart)
+      console.log(document.activeElement)
+      console.log(document.activeElement.value)
+     })
+    
     let content = this.getContent()
 
     let items = []
     for (let i = 0; i < 1000; i++) {
-      items.push(<div id={"btn-" + i} style={{position:"static"}}><Popover placement="rightTop" title={'text'} content={content} trigger="click"
+      let child = (<Popover placement="rightTop" title={'text'} content={content} trigger="click"
       getPopupContainer={function() {
-        return document.getElementById('btn-' + i)
+        console.log('getPopupCaonianter')
+        return document.getElementById("popup-"+i)
       }} style={{position: "static"}} id={"popup-"+i}>
       <Button size="small" type='primary'><Icon type='setting'/>RT</Button>
-    </Popover></div>)
+    </Popover>)
+
+    let outer = (<div id={"btn-" + i} style={{position:"relative"}} key={"btn-"+i}>
+      <div id={"popup-"+i}/>
+      {child}
+    </div>)
+
+      items.push(outer)
     }
     return (
       <div className="lab" style={{overflow:"scroll", display: "flex", height:500}}>
+      <React.Fragment>
         {items}
+        </React.Fragment>
       </div>
     );
   }
